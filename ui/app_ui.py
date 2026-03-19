f9663498e51eae21e176d2498a45a04131973323
kncabatuan <kncabatuan@yahoo.com>
2026-03-19 10:51:05 +0800

    Add frames for image and game details display

--- ui/app_ui.py
+++ ui/app_ui.py
@@ -11,7 +11,7 @@ class AppUI:
         self.list_box = tk.Listbox
         self.list_box_height = 0
         self.main_button = tk.Button
-        self.main_display = tk.Label
+        self.status_display = tk.Label
         self.create_widgets()
     
     def config_window(self) -> None:
@@ -19,30 +19,42 @@ class AppUI:
         screen_height = self.root.winfo_screenheight()
         screen_width = self.root.winfo_screenwidth()
         y = (screen_height - 600) // 2
-        x = (screen_width - 500) // 2
-        self.root.geometry(f"500x500+{x}+{y}")
+        x = (screen_width - 1000) // 2
+        self.root.geometry(f"1000x500+{x}+{y}")
         self.root.resizable(False, False)
 
+    def create_main_frames(self) -> None:
+        self.root.columnconfigure(0, weight=1, uniform="group1")
+        self.root.columnconfigure(1, weight=1, uniform="group1")
+        self.root.rowconfigure(0, weight=1)
+        self.left_frame = tk.Frame(self.root, bd=1, relief="solid")
+        self.right_frame = tk.Frame(self.root, bd=1, relief="solid")
+        self.left_frame.grid(row=0, column=0, sticky="nsew")
+        self.right_frame.grid(row=0, column=1, sticky="nsew")
+
     def create_main_header(self) -> None:
-        main_label = tk.Label(self.root, text="Welcome to GameHype!", font=("Arial", 18))
+        main_label_frame = tk.Frame(self.left_frame)
+        main_label_frame.pack(fill="x", pady=(10,30))
+
+        main_label = tk.Label(main_label_frame, text="Welcome to GameHype!", font=("Arial", 18))
         main_label.pack(pady=40)
 
     def create_sub_header(self) -> None:
-        sub_header_frame = tk.Frame(self.root)
-        sub_header_frame.pack(pady=(0, 30))
+        sub_header_frame = tk.Frame(self.left_frame)
+        sub_header_frame.pack(fill="x", pady=(0, 30))
 
         sub_label = tk.Label(sub_header_frame, text="Please select a game to hype check", font=("Arial", 12))
         sub_label.pack()
 
     def create_entry(self) -> None:
-        entry_frame = tk.Frame(self.root, height=80, width=400)
+        entry_frame = tk.Frame(self.left_frame, height=80)
         entry_frame.pack_propagate(False)
-        entry_frame.pack(pady=(0,20))
+        entry_frame.pack(fill="x", pady=(0,20), padx=20)
 
         self.entry_box = tk.Entry(entry_frame, font=("Arial", 10))
         self.entry_box.pack(side="top", fill="x", padx=10)
 
-        self.list_box_frame = tk.Frame(entry_frame)
+        self.list_box_frame = tk.Frame(entry_frame, bd=2, relief="ridge")
 
         self.list_box = tk.Listbox(
             self.list_box_frame, 
@@ -64,8 +76,8 @@ class AppUI:
         self.list_box.bind("<<ListboxSelect>>", self.controller.on_listbox_click)
 
     def create_main_button(self) -> None:
-        main_button_frame = tk.Frame(self.root)
-        main_button_frame.pack(pady=(0, 30))
+        main_button_frame = tk.Frame(self.left_frame)
+        main_button_frame.pack(fill="x", pady=(0, 30))
 
         self.main_button = tk.Button(
             main_button_frame, 
@@ -82,20 +94,40 @@ class AppUI:
         self.main_button.bind("<Enter>", lambda e: self.main_button.config(bg="#d0d0d0"))
         self.main_button.bind("<Leave>", lambda e: self.main_button.config(bg="#e0e0e0"))
 
-    def create_main_display(self) -> None:
-        main_display_frame = tk.Frame(self.root)
-        main_display_frame.pack(pady=(10, 30))
+    def create_status_display(self) -> None:
+        status_display_frame = tk.Frame(self.left_frame)
+        status_display_frame.pack(fill="x", pady=(10, 30))
 
-        self.main_display = tk.Label(main_display_frame, text=self.controller.display, font=("Arial", 12))
-        self.main_display.pack()
+        self.status_display = tk.Label(status_display_frame, text=self.controller.display, font=("Arial", 12))
+        self.status_display.pack()
+
+    def create_image_display(self) -> None:
+        image_display_frame = tk.Frame(self.right_frame, height=250, bd=2, relief="ridge")
+        image_display_frame.pack_propagate(False)
+        image_display_frame.pack(fill="x", pady=(10, 30))
+
+        image_display_header = tk.Label(image_display_frame, text="Game:", font=("Arial", 12))
+        image_display_header.pack(side="top", pady=5)
+
+        image_frame = tk.Frame(image_display_frame)
+        image_frame.pack_propagate(False)
+        image_frame.pack(fill="both", expand=True, padx=5, pady=5)
+    
+    def create_game_details_display(self) -> None:
+        game_details_frame = tk.Frame(self.right_frame, height=250, bd=2, relief="ridge")
+        game_details_frame.pack_propagate(False)
+        game_details_frame.pack(pady=(10,30), fill="x")
 
     def create_widgets(self) -> None:
         self.config_window()
+        self.create_main_frames()
         self.create_main_header()
         self.create_sub_header()
         self.create_entry()
         self.create_main_button()
-        self.create_main_display()
+        self.create_status_display()
+        self.create_image_display()
+        self.create_game_details_display()
 
 
 def open_ui() -> None:
