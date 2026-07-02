a6ab7fb7da34c74f5acbb81bdaefc77d1fb505d0
kncabatuan <kncabatuan@yahoo.com>
2026-07-02 15:15:56 +0800

    WIP Unit testing. Implement unit test for wrong or no api key

---
+++ tests/test_rawg_service.py
@@ -0,0 +1,19 @@
+from api import rawg_service
+from unittest.mock import patch
+import pytest
+import requests
+
+def test_no_api_key():
+    test_game_title = "Stardew Valley"
+
+    with patch('api.rawg_service.api_key', None):
+        assert rawg_service.get_game_titles(test_game_title) == []
+
+def test_wrong_api_key():
+    test_game_title = "Stardew Valley"
+
+    with patch('api.rawg_service.api_key', 'wrong_key'):
+        with patch('api.rawg_service.requests.get') as mock_get:
+            mock_get.side_effect = requests.exceptions.HTTPError("401 Client Error: Unauthorized")
+        
+            assert rawg_service.get_game_titles(test_game_title) == []
