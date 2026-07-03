82a7df74314016fc7273fb4d832ef539beb6f7f1
kncabatuan <kncabatuan@yahoo.com>
2026-07-03 15:51:52 +0800

    Add unit test for no game, WIP unit test for no key and wrong key for get_game_details

--- tests/test_rawg_service.py
+++ tests/test_rawg_service.py
@@ -2,7 +2,7 @@ from api import rawg_service
 from unittest.mock import patch, Mock
 import requests
 
-
+# Test cases for get_game_titles
 def test_empty_title():
     test_game_title = ""
     assert rawg_service.get_game_titles(test_game_title) == []
@@ -78,3 +78,32 @@ def test_get_game_titles():
 
         assert rawg_service.get_game_titles(test_game_title) == ['stardew valley 1', 'stardew valley 2', 'stardew valley 3']
 
+# Test cases for get_game_details
+def test_no_game():
+    test_game = ""
+    assert rawg_service.get_game_details(test_game) == {}
+
+def test_no_api_key_for_details():
+    test_game = "Stardew Valley"
+
+    with patch('api.rawg_service.api_key', None):
+        assert rawg_service.get_game_details(test_game) == {}
+
+    with patch('api.rawg_service.requests.get') as mock_get:
+        mock_get = Mock()
+        mock_get.json.return_value = {}
+        mock_get.raise_for_status.return_value = None
+        
+
+def test_wrong_api_key_for_details():
+    test_game = "Stardew Valley"
+
+    with patch('api.rawg_service.api_key', 'wrong_key'):
+        with patch('api.rawg_service.requests.get') as mock_get:
+            mock_get.side_effect = requests.exceptions.HTTPError("401 Client Error: Unauthorized")
+
+            assert rawg_service.get_game_details(test_game) == {}
+
+#Test no results
+#Test no id
+#Test success
