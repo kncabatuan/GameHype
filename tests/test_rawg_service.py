532c5678d66088820496105d3fbdbdcab915f6e2
kncabatuan <kncabatuan@yahoo.com>
2026-07-03 14:44:08 +0800

    Add unit test for empty title, no results, and no name key

--- tests/test_rawg_service.py
+++ tests/test_rawg_service.py
@@ -1,8 +1,11 @@
 from api import rawg_service
 from unittest.mock import patch, Mock
-import json
-import pytest
 import requests
+
+
+def test_empty_title():
+    test_game_title = ""
+    assert rawg_service.get_game_titles(test_game_title) == []
 
 def test_no_api_key():
     test_game_title = "Stardew Valley"
@@ -27,6 +30,36 @@ def test_request_exception():
 
         assert rawg_service.get_game_titles(test_game_title) == []
 
+def test_no_results():
+    test_game_title = "Stardew Valley"
+    test_game_response_data = {}
+
+    with patch("api.rawg_service.requests.get") as mock_get:
+        mock_response = Mock()
+        mock_response.json.return_value = test_game_response_data
+        mock_response.raise_for_status.return_value = None
+        mock_get.return_value = mock_response
+
+        assert rawg_service.get_game_titles(test_game_title) == []
+
+def test_no_name_key_in_results():
+    test_game_title = "Stardew Valley"
+    test_response_data = {
+        'results': [
+            {'name': 'stardew valley 1'}, 
+            {'name': 'stardew valley 2'}, 
+            {'id': 3}
+            ]
+        }
+
+    with patch("api.rawg_service.requests.get") as mock_get:
+        mock_response = Mock()
+        mock_response.json.return_value = test_response_data
+        mock_response.raise_for_status.return_value = None
+        mock_get.return_value = mock_response
+
+        assert rawg_service.get_game_titles(test_game_title) == ['stardew valley 1', 'stardew valley 2', 'Unknown Game']
+
 def test_get_game_titles():
     test_game_title = "Stardew Valley"
     test_response_data = {
@@ -44,3 +77,4 @@ def test_get_game_titles():
         mock_get.return_value = mock_response
 
         assert rawg_service.get_game_titles(test_game_title) == ['stardew valley 1', 'stardew valley 2', 'stardew valley 3']
+
