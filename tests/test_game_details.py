18ba8d1ec437f3d8cdc6b6c5276406bf447b7914
kncabatuan <kncabatuan@yahoo.com>
2026-09-15 10:19:58 +0800

    Add unit test for game class init success

---
+++ tests/test_game_details.py
@@ -0,0 +1,37 @@
+from models import game_details
+
+
+def test_game_class_init_success():
+    test_raw_data = {
+        "name": "Test Game",
+        "ratings": [
+            {
+                "id": 5,
+                "title": "exceptional",
+                "count": 100,
+                "percent": 100
+            }
+        ]
+    }
+
+    game = game_details.Game(test_raw_data)
+
+    assert game.raw_data == test_raw_data
+    assert game.title == "Test Game"
+    assert game.rawg_ratings == [
+            {
+                "id": 5,
+                "title": "exceptional",
+                "count": 100,
+                "percent": 100
+            }
+        ]
+    
+
+
+
+
+
+
+
+
