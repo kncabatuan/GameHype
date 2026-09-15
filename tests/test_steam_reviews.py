ee31d3a270907677b0821cb61cbcb7bede35b706
kncabatuan <kncabatuan@yahoo.com>
2026-09-15 13:56:14 +0800

    Add unit test for get steam reviews no query summary

--- tests/test_steam_reviews.py
+++ tests/test_steam_reviews.py
@@ -143,3 +143,24 @@ def test_get_steam_reviews_request_exception():
 
             mock_get_steam_id.assert_called_once_with(test_game_title)
             assert reviews_data == None
+
+
+def test_get_steam_reviews_no_query_summary():
+    test_game_title = "Test Game"
+    test_reviews_url = f"https://store.steampowered.com/appreviews/1000?json=1&num_per_page=100"
+    test_reviews_data = {}
+
+    with patch("api.steam_reviews.get_steam_id") as mock_get_steam_id:
+        mock_get_steam_id.return_value = 1000
+
+        with patch("requests.get") as mock_get:
+            mock_response = Mock()
+            mock_response.json.return_value = test_reviews_data
+            mock_response.raise_for_status.return_value = None
+            mock_get.return_value = mock_response
+
+            reviews_data = steam_reviews.get_steam_reviews(test_game_title)
+
+            mock_get.assert_called_once_with(test_reviews_url)
+            mock_get_steam_id.assert_called_once_with(test_game_title)
+            assert reviews_data == {}
