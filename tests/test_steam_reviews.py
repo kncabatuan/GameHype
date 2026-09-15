c131618fb5c09c37ea9d1056e36514553537cd1b
kncabatuan <kncabatuan@yahoo.com>
2026-09-15 13:50:48 +0800

    Add unit test for get steam reviews success

--- tests/test_steam_reviews.py
+++ tests/test_steam_reviews.py
@@ -88,3 +88,29 @@ def test_get_steam_id_no_id():
 
         mock_get.assert_called_once_with(test_steamid_url)
         assert steam_id == None
+
+
+def test_get_steam_reviews_success():
+    test_game_title = "Test Game"
+    test_reviews_url = f"https://store.steampowered.com/appreviews/1000?json=1&num_per_page=100"
+    test_reviews_data = {
+        "query_summary": {
+            "total_positive": 100,
+            "total_negative": 50,
+        }
+    }
+
+    with patch("api.steam_reviews.get_steam_id") as mock_get_steam_id:
+        mock_get_steam_id.return_value = 1000
+
+        with patch("requests.get") as mock_get:
+            mock_response = Mock()
+            mock_response.json.return_value = test_reviews_data
+            mock_response.raise_for_status.return_value = None
+            mock_get.return_value = mock_response
+
+            reviews_data = steam_reviews.get_steam_reviews(test_game_title)
+
+            mock_get.assert_called_once_with(test_reviews_url)
+            mock_get_steam_id.assert_called_once_with(test_game_title)
+            assert reviews_data == test_reviews_data["query_summary"]
