8e5393db414a1819776442a202e65f71bc51abc7
kncabatuan <kncabatuan@yahoo.com>
2026-09-15 13:43:18 +0800

    Add unit test for get steam id no items

--- tests/test_steam_reviews.py
+++ tests/test_steam_reviews.py
@@ -47,3 +47,20 @@ def test_get_steam_id_request_exception():
         steam_id = steam_reviews.get_steam_id(test_game_title)
 
         assert steam_id == None
+
+
+def test_get_steam_id_no_items_in_response():
+    test_game_title = "Test Game"
+    test_steamid_url = f"https://store.steampowered.com/api/storesearch/?term={test_game_title}&l=english&cc=US"
+    test_response_data = {}
+
+    with patch("requests.get") as mock_get:
+        mock_response = Mock()
+        mock_response.json.return_value = test_response_data
+        mock_response.raise_for_status.return_value = None
+        mock_get.return_value = mock_response
+
+        steam_id = steam_reviews.get_steam_id(test_game_title)
+
+        mock_get.assert_called_once_with(test_steamid_url)
+        assert steam_id == None
